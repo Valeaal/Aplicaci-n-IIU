b709fbb72081acf23cdf150dd3b99c74e18571fb
Valenciaal <valeaal@uma.es>
2024-04-12 12:37:54 +0200

    Nuevos componentes

---
+++ app_code/Frontend/src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function navbar(){
+
+    /* Como esto tiene que ser responsivo, propongo introducir aqui el codigo html para
+    importar el navbar de Bootstrap */
+
+}
