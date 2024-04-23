e3e3c1152361bbfea10c98e1d3310a65975279b7
Valenciaal <valeaal@uma.es>
2024-04-24 00:29:51 +0200

    Conexión a la BDD

--- app_code/Backend/bbdd.js
+++ app_code/Backend/bbdd.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('interfaces_bbdd', 'root', '1234', {
+const sequelize = new Sequelize('interfaces_bbdd', 'IIU', 'IIU_bdd', {
     host: 'localhost',
     dialect: 'mysql'
   });
