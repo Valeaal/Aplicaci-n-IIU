669f8454075290a50b95b4e5ffc4ff2167c3c6e2
Valenciaal <valeaal@uma.es>
2024-05-15 01:14:52 +0200

    Mejor visiblidad home responsive

--- app_code/Frontend/src/components/tabs/home.jsx
+++ app_code/Frontend/src/components/tabs/home.jsx
@@ -45,29 +45,29 @@ const Everyone = () => {
                 {/* Primera fila de elementos */}
                 <div className='col-lg-8'>
                 <h2>Nuestro centro:</h2>
-                    <div className='d-flex flex-column main-content'>
+                    <div className='d-flex flex-column main-conten mb-5'>
                         <Carroussel />
 
                     </div>
                 </div>
                 <div className='col-lg-4'>
-                    <div className='noticias-section'>
+                    <div className='noticias-section mb-5'>
                         <h2>Últimas noticias:</h2>
                         <News />
                     </div>
                 </div>
             </div>
-            <div className='row mx-2 mt-5'>
+            <div className='row mx-2'>
                 {/* Segunda fila de elementos */}
                 <div className='col-lg-8'>
                     <h2>Encuéntranos:</h2>
-                    <section className='map-section'>
+                    <section className='map-section mb-5'>
                         <Map />
                     </section>
                 </div>
                 <div className='col-lg-4'>
                     <h2>Escríbanos:</h2>
-                    <section className='application-section'>
+                    <section className='application-section mb-5'>
                         <Application />
                     </section>
                 </div>
