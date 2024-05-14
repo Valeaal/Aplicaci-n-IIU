44df6ca6688cf12c36a2afd038d10a0963ca4597
Valenciaal <valeaal@uma.es>
2024-05-14 13:44:21 +0200

    OTRO cambio en las contraseñas del puñetero login

--- app_code/Backend/Routes/usuarioRoutes.js
+++ app_code/Backend/Routes/usuarioRoutes.js
@@ -36,7 +36,7 @@ router.post('/process-login', async (req, res) => {
         // Comparar la contraseña ingresada con la contraseña almacenada en la base de datos
         user_password= await bcrypt.hash(password, 10); //mientras no las guardemos cifradas
         //user_password= user.password;
-        const isPasswordValid = await bcrypt.compare(user.password, user_password);      //bcrypt comprueba el hash de la contraseña
+        const isPasswordValid = await bcrypt.compare(password, user.password);      //bcrypt comprueba el hash de la contraseña
         console.log(chalk.blue("Contraseña introducida:", password))
         console.log(chalk.blue("Contraseña introducida (hash):", user_password))
         console.log(chalk.blue("Contraseña de la bdd (hash):", user.password))
