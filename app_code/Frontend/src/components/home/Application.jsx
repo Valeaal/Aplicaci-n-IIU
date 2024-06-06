d4f735a7528fcc1184436405b2d92b5e3e7173d3
Valenciaal <valeaal@uma.es>
2024-06-07 00:38:06 +0200

    Escríbanos funcional

--- app_code/Frontend/src/components/home/Application.jsx
+++ app_code/Frontend/src/components/home/Application.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Application(){
 
+    const [message, setMessage] = useState("");
+    
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        // Aquí puedes implementar el código para enviar el mensaje por correo electrónico
+        const emailAddress = 'vinmaculadadireccion@gmail.com';
+        const subject = '[WEB]';
+        const emailBody = `${message}`;
+        const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(emailBody)}`;
+        window.location.href = mailtoLink;
+    }
+
     const stylePH = {
-        width:"100%",
-        height:200
+        width: "100%",
+        height: 200
     }
 
     const styleButton = {
-        width:'40%', 
-        margin:'2px'
+        width: '40%',
+        margin: '2px'
     }
 
-    return(
+    return (
         <>
             <div className='d-flex flex-column align-items-center align-self-center'>
-                <textarea placeholder='Aquí puede mandar un mensaje. Por favor, escriba su nombre para saber quién es el emisor.' style={stylePH}></textarea>
-                <Button aria-label='Enviar la sugerencia administración' variant="success" className='mt-2 mb-2'>Enviar a administración</Button>
+                <textarea placeholder='Aquí puede mandar un mensaje usando el correo electrónico de la escuela.' style={stylePH} value={message} onChange={handleChange}></textarea>
+                <Button aria-label='Enviar la sugerencia administración' variant="success" className='mt-2 mb-2' onClick={handleSubmit}>Enviar a administración</Button>
             </div>
         </>
     );
